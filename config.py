944eeee1bcad01344342e9c8d01b9cbdc49154f1
whoami847 <mrbeastbangla069@gmail.com>
2025-04-11 03:12:52 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
-# Load API_TOKEN from the environment variable
-API_TOKEN = os.getenv("API_TOKEN")  # This will get the value from Koyeb's environment variables
+# Telegram API ID and API Hash (required for Client API)
+API_ID = 21463947  # Directly assign your API ID
+API_HASH = "39d6a5245f670ee68c507e274b3c7b3d"  # Directly assign your API Hash
+
+# Telegram Bot API Token
+API_TOKEN = "8018682528:AAEB0W9Ljb8Prfyi9qeVYX93OZzcGBuLEYA"  # Directly assign your Bot API Token from BotFather
 
 if not API_TOKEN:
     raise ValueError("API_TOKEN is not set in environment variables")
