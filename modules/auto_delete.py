d95edc853f0e7f6bcf8fffe00a5e9217b333cae2
whoami847 <mrbeastbangla069@gmail.com>
2025-04-11 11:10:28 +0600

    Update auto_delete.py

--- modules/auto_delete.py
+++ modules/auto_delete.py
@@ -1,12 +1,5 @@
-import time
-from threading import Timer
+import asyncio
 
 async def auto_delete_handler(client, message):
-    delete_time = 20 * 60  # 20 minutes in seconds
-    Timer(delete_time, delete_message, [message]).start()
-
-def delete_message(message):
-    try:
-        message.delete()
-    except Exception as e:
-        print(f"Failed to delete message: {e}")
+    await asyncio.sleep(1200)  # 20 মিনিট অপেক্ষা করুন
+    await message.delete()
